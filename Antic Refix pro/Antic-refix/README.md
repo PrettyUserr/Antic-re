d3360b7bfa13c587b8a88ca75741ff7e2ea67438
PrettyUserr <114989377+PrettyUserr@users.noreply.github.com>
2023-01-29 14:59:44 +0100

    Add files via upload

---
+++ Antic Refix pro/Antic-refix/README.md
@@ -0,0 +1,1 @@
+# Antic-refix
---
+++ Antic Refix pro/main.js
@@ -0,0 +1,24 @@
+ 
+
+ 
+ 
+// function magicjava() {
+//      alert ("i have been coding since 12")
+// }
+
+
+//  console.log
+ var hamburgerdiv  = document.querySelector(".hamburger")
+
+
+function magicjava() {
+      hamburgerdiv.classList.toggle("showburger")
+ }
+
+
+
+ var swiper = new Swiper({
+     slidesPerView: 3,
+     spaceBetween:30,
+     
+ })
